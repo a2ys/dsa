a08778b2c891feb561e5fd9015e3b52ce84508c4
Aayush Shukla <oreus.aks@gmail.com>
2024-04-12 20:54:11 +0530

    Creating the file for LinkedList

---
+++ basic/LinkedList.java
@@ -0,0 +1,15 @@
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    public LinkedList() {
+
+    }
+}
